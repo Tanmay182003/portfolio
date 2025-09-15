e95b4f567f2330e866f40280e15d8d7cfe4c3894
Tanmay182003 <singlat@purdue.edu>
2025-09-15 15:11:02 -0400

    Fix JavaScript variable conflict in navigation loops

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,14 +144,14 @@ const pages = document.querySelectorAll("[data-page]");
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase().trim() === pages[i].dataset.page.toLowerCase()) {
-        pages[i].classList.add("active");
+    for (let j = 0; j < pages.length; j++) {
+      if (this.innerHTML.toLowerCase().trim() === pages[j].dataset.page.toLowerCase()) {
+        pages[j].classList.add("active");
         navigationLinks[i].classList.add("active");
         window.scrollTo(0, 0);
       } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
+        pages[j].classList.remove("active");
+        navigationLinks[j].classList.remove("active");
       }
     }
 
